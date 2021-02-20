a9bd4d2ae650b27b5f3bade077776184aa150035
Henriquepkos <79348191+Henriquepkos@users.noreply.github.com>
2021-02-20 15:27:14 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,293 +1,234 @@
 const help = (prefix) => {
-	return `    *HENRIQUE O BRABO 🔥*
+	return `    
 
-             *PK BOT NA ÁREA* 😈
+  ㅤㅤㅤ━━━━━━━✦✗✦━━━━━━━━
+           *🔥𝐻𝐸𝑁𝑅𝐼𝑄𝑈𝐸 𝐷𝑂𝑀𝐼𝑁𝐴*🔥
+  ㅤㅤㅤ━━━━━━━✦✗✦━━━━━━━━
+   
+         
+           ──────  ─────────
+           ╔═╗╔╦╗  ╔╗────╔╗─      
+           ║╬║║╔╝  ║╚╗╔═╗║╚╗
+           ║╔╝║╚╗  ║╬║║╬║║╔╣
+           ╚╝─╚╩╝  ╚═╝╚═╝╚═╝
+           ──────  ─────────
 
-*PK BOT AINDA ESTA EM DESENVOLVIMENTO!*
 
+   *🔥𝙲𝚁𝙸𝙰𝙳𝙾𝚁 𝙳𝙾 𝙱𝙾𝚃:* wa.me/556993733829
 
+   *🔥𝙳𝙾𝙽𝙾 𝙳𝙾 𝙱𝙾𝚃:* wa.me/558994524151
 
-CANAL DO CRIADOR DISSO TUDO: https://youtube.com/channel/UCZEtf9AlsC2zsJQwrfW-44w
+   *🔥𝙲𝙰𝙽𝙰𝙻 𝙳𝙾 𝙽𝙰𝙱𝚄𝚃𝙴:* https://youtube.com/channel/UCZEtf9AlsC2zsJQwrfW-44w
 
 
    [𝑨𝑳𝑮𝑼𝑵𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑵𝑨̃𝑶 𝑬𝑺𝑻𝑨̃𝑶 𝑭𝑼𝑵𝑪𝑰𝑶𝑵𝑨𝑵𝑫𝑶, 𝑺𝑬 𝑨𝑪𝑯𝑨𝑹 𝑹𝑼𝑰𝑴 𝑬́ 𝑺𝑰𝑴𝑷𝑳𝑬𝑺 𝑷𝑨𝑹𝑨 𝑫𝑬 𝑼𝑺𝑨𝑹, 𝑮𝑹𝑨𝑻𝑶!]
 
-
-
  
-    𝑩𝑬𝑴 𝑽𝑰𝑵𝑫𝑶 𝑨𝑶 𝑴𝑬𝑵𝑼 𝑫𝑶 𝑩𝑶𝑻🤭
-
-
-
- 
-         【 *INÚTEIS* 】 :
-
-  ➡ ${prefix}blocklist
-     【LISTA DE BLOCK】
-  ➡ ${prefix}chatlist
-     【LISTA CHAT】❌
-  ➡ ${prefix}ping
-     【PING】
-  ➡ ${prefix}bugreport
-     【REPORTA BUG】❌
-
-
-      
-      *ZOAÇÃO* :
-
-
-  ➡ ${prefix}%GADO
-   
-  ➡ ${prefix}%CORNO(A)
-
-  ➡ ${prefix}%NAMORARESSEANO
-
-  ➡ ${prefix}%GAY
-
-
-
-
-    【 *MAIS USADOS RECENTEMENTE* 】
-  
-  ➡ ${prefix}sticker
-    【FAZER FIGURINHA】
-  ➡ ${prefix}toimg
-    【CONVERTE FIGURINHA EM IMAGEM】
-  ➡ ${prefix}tomp3
-    【PEGAR ÁUDIO DE ALGUM VÍDEO】
-
- 
-
-     【FOTOS ANIME/LOLI e etc】
- 
-
-  ➡${prefix}loli
-  【foto de loli】
-
-  ➡${prefix}nekoanime
-   【NEKO ANIME】
-
-  ➡${prefix}randomanime
-   【anime aleatório】
-
-
-  ➡${prefix}pokémon
-   【foto de pokémon】
-
-
-   
-
-
-     
-
-
-        *MELHORES SOBRE ÁUDIO*
- 
-  ➡ ${prefix}play nome da música
-    【DOWNLOAD MÚSICA】
-  ➡ ${prefix}tts pt texto
-    【FALAR SEU TEXTO】
-
-
-
-      +18 *NÃO USE NO GRUPO SUJEITO A BAN*
-
-  
-  ➡${prefix}hentai
-    【foto hentai】
-
-  ➡${prefix}randomhentaio
-    【random hentai】
-
-
-
-
-
-
-      
-
-         【 𝑶𝑼𝑻𝑹𝑶𝑺 】
-  
-  ➡ ${prefix}send
-  ➡ ${prefix}wame
-  ➡ ${prefix}exe
-  ➡ ${prefix}qrcode
-  ➡ ${prefix}afk
-  ➡ ${prefix}timer
-  ➡ ${prefix}fml
-  ➡ ${prefix}fml2
-
-
-
-
-
-
-
-          【 *FONTES* 】
-
-  ➡ ${prefix}bpink
-  ➡ ${prefix}marvellogo
-  ➡ ${prefix}snowwrite
-  ➡ ${prefix}3dtext
-  ➡ ${prefix}ninjalogo
-  ➡ ${prefix}water
-  ➡ ${prefix}firetext
-  ➡ ${prefix}logowolf
-  ➡ ${prefix}logowolf2
-  ➡ ${prefix}phlogo
-  ➡ ${prefix}glitch        𝑨 𝑴𝑨𝑰𝑶𝑹𝑰𝑨 𝑵𝑨̃𝑶 𝑷𝑬𝑮𝑨         
-  ➡ ${prefix}neonlogo
-  ➡ ${prefix}neonlogo2
-  ➡ ${prefix}lionlogo
-  ➡ ${prefix}jokerlogo
-  ➡ ${prefix}shadow
-  ➡ ${prefix}burnpaper
-  ➡ ${prefix}coffee
-  ➡ ${prefix}lovepaper
-  ➡ ${prefix}woodblock
-  ➡ ${prefix}qowheart
-  ➡ ${prefix}mutgrass
-  ➡ ${prefix}undergocean
-  ➡ ${prefix}woodenboards
-  ➡ ${prefix}wolfmetal
-  ➡ ${prefix}metalictglow
-  ➡ ${prefix}8bit
-  ➡ ${prefix}ttp
-  ➡ ${prefix}herrypotter
-  ➡ ${prefix}pubglogo
-  ➡ ${prefix}quotemaker
-        
-       【 *MEDIA* 】
-  
-
-  ➡ ${prefix}trendtwit
-  ➡ ${prefix}randomkpop
-     【FOTOS ALEATÓRIA DE KPOP🤢】
-  ➡ ${prefix}ytsearch
-    【PESQUISA】
-      
-
-
-      【  *EDUCAÇÃO* 】
-  
-  ➡ ${prefix}wiki
-  ➡ ${prefix}wikien
-  ➡ ${prefix}nulis
-  ➡ ${prefix}quotes
-  ➡ ${prefix}quotes2
-  ➡ ${prefix}artinama
-
-
-     【 *DOWNLOADER* 】
-  
-  ➡ ${prefix}images
-    【IMAGENS ALEATÓRIA】
-  ➡ ${prefix}ytmp3
-    【MP3】
-  ➡ ${prefix}ytmp4
-    【DOWNLOAD MP4(VIDEO)】
-  ➡ ${prefix}tiktok
-  ➡ ${prefix}joox
-         
-
-           *MEME*
-      
-
-
-  ➡  ${prefix}meme
-  ➡ 【MEME INDONÉSIO】
-  ➡  ${prefix}memeindo
-  ➡ 【MEMEINDO INDONESIO】
- 
-
-
-
-
-     【 *MÚSICA* 】
-  
-  ➡ ${prefix}lirik
-  ➡ ${prefix}chord
-
-
-
-       ➡ *ISLAM*
-  
-   ➡ ${prefix}quran
-
-
-
-
-
-       【 *STALK* 】
-  
-  
-  ➡ ${prefix}tiktokstalk
-     【stalkear tik tok】
-  ➡ ${prefix}igstalk
-
-
-
-
-
-        【 *WIBU* 】
-
-  
-  ➡ ${prefix}neonime
-  ➡ ${prefix}pokemon
-  ➡ ${prefix}loli
-  ➡ ${prefix}waifu
-  ➡ ${prefix}randomanime
-  ➡ ${prefix}husbu
-  ➡ ${prefix}husbu2
-  ➡ ${prefix}wait
-  ➡ ${prefix}nekonime
-
-
-
-      【 *DIVERSÃO* 】
-  
-  ➡ ${prefix}alay
-  ➡ ${prefix}gantengcek
-  ➡ ${prefix}watak
-  ➡ ${prefix}hobby
-  ➡ ${prefix}game
-  ➡ ${prefix}bucin
-  ➡ ${prefix}trust
-  ➡ ${prefix}dare
-  ➡ ${prefix}simi
-
-
-
-
-
-         【 *INFORMAÇÃO* 】
-  
-  ➡ ${prefix}bahasa
-     【SABER TODOS CÓDIGO DE PAIS】
-  ➡ ${prefix}kodenegara
-  ➡ ${prefix}kbbi
-  ➡ ${prefix}fakta
-  ➡ ${prefix}infocuaca
-  ➡ ${prefix}infogempa
-  ➡ ${prefix}jadwaltvnow
-  ➡ ${prefix}covid
-
-
-          【 SÓ FUNCIONA COM O DONO 】
-
-
-  
-   ➭ ${prefix}setprefix
-     【MUDAR PREFIXO】
-   ➭ ${prefix}block
-     【BLOQUEAR】
-   ➭ ${prefix}bc
-     【TM】
-   ➭ ${prefix}bcgc
-     【TM SÓ PARA MEMBROS DO GP】
-   ➭ ${prefix}clearall
-     【APAGAR TODAS MSG】
-
+    𝘽𝙀𝙈 𝙑𝙄𝙉𝘿𝙊 𝘼𝙊 𝙈𝙀𝙉𝙐 𝘿𝙊 𝙋𝙆 𝘽𝙊𝙏🔥
+
+
+╔═════════════════════════
+║          *🔥𝙸𝙽𝚄𝚃𝙴𝙸𝚂🔥* 
+║  ➩ ${prefix}blocklist
+║     【LISTA DE BLOCK】
+║  ➩ ${prefix}chatlist
+║     【LISTA CHAT】❌
+║  ➩ ${prefix}ping
+║     【PING】
+║  ➩ ${prefix}bugreport
+║     【REPORTA BUG】❌
+╠═════════════════════════               
+║      *🔥𝚉𝙾𝙰𝙲𝙰𝙾🔥* 
+║
+║  ➩ ${prefix}%GADO
+║   
+║  ➩ ${prefix}%CORNO(A)
+║
+║  ➩ ${prefix}%NAMORARESSEANO
+║
+║  ➩ ${prefix}%GAY
+╠═════════════════════════
+║ *🔥𝙼𝙰𝙸𝚂 𝚄𝚂𝙰𝙳𝙾𝚂 𝚁𝙴𝙲𝙴𝙽𝚃𝙴𝙼𝙴𝙽𝚃𝙴🔥*
+║
+║  ➩ ${prefix}sticker
+║    【FAZER FIGURINHA】
+║  ➩ ${prefix}toimg
+║  【CONVERTE FIGURINHA EM IMAGEM】
+║  ➩ ${prefix}tomp3
+║    【PEGAR ÁUDIO DE ALGUM VÍDEO】╠═════════════════════════
+║ *🔥𝙵𝙾𝚃𝙾𝚂 𝙰𝙽𝙸𝙼𝙴/𝙻𝙾𝙻𝙸 𝙴 𝙴𝚃𝙲.🔥*
+║
+║ ➩ ${prefix}loli
+║   【foto de loli】
+║ ➩ ${prefix}nekoanime
+║   【NEKO ANIME】
+║ ➩ ${prefix}randomanime
+║   【anime aleatório】
+║ ➩ ${prefix}pokémon
+║   【foto de pokémon】
+╠═════════════════════════
+║   *🔥𝙼𝙴𝙻𝙷𝙾𝚁𝙴𝚂 𝚂𝙾𝙱𝚁𝙴 𝙰𝚄𝙳𝙸𝙾🔥*
+║
+║  ➩ ${prefix}play nome da música
+║    【DOWNLOAD MÚSICA】
+║  ➩ ${prefix}tts pt texto
+║    【FALAR SEU TEXTO】
+╠═════════════════════════
+║          *🔥+18🔥*
+║  
+║  ➩ ${prefix}hentai
+║    【foto hentai】
+║
+║  ➩ ${prefix}randomhentaio
+║    【random hentai】╠═════════════════════════
+║         *🔥𝙾𝚄𝚃𝚁𝙾𝚂🔥* 
+║  
+║  ➩ ${prefix}send
+║  ➩ ${prefix}wame
+║ ➩ ${prefix}exe
+║  ➩ ${prefix}qrcode
+║  ➩ ${prefix}afk
+║  ➩ ${prefix}timer
+║  ➩ ${prefix}fml
+║  ➩ ${prefix}fml2
+╠═════════════════════════
+║            *🔥𝙵𝙾𝙽𝚃𝙴𝚂🔥*
+║
+║  ➩ ${prefix}bpink
+║  ➩ ${prefix}marvellogo
+║  ➩ ${prefix}snowwrite
+║  ➩ ${prefix}3dtext
+║  ➩ ${prefix}ninjalogo
+║  ➩ ${prefix}water
+║  ➩ ${prefix}firetext
+║  ➩ ${prefix}logowolf
+║  ➩ ${prefix}logowolf2
+║  ➩ ${prefix}phlogo
+║  ➩ ${prefix}glitch                 
+║  ➩ ${prefix}neonlogo
+║  ➩ ${prefix}neonlogo2
+║  ➩ ${prefix}lionlogo
+║  ➩ ${prefix}jokerlogo
+║  ➩ ${prefix}shadow
+║  ➩ ${prefix}burnpaper
+║  ➩ ${prefix}coffee
+║  ➩ ${prefix}lovepaper
+║  ➩ ${prefix}woodblock
+║  ➩ ${prefix}qowheart
+║  ➩ ${prefix}mutgrass
+║  ➩ ${prefix}undergocean
+║  ➩ ${prefix}woodenboards
+║  ➩ ${prefix}wolfmetal
+║  ➩ ${prefix}metalictglow
+║  ➩ ${prefix}8bit
+║  ➩ ${prefix}ttp
+║  ➩ ${prefix}herrypotter
+║  ➩ ${prefix}pubglogo
+║  ➩ ${prefix}quotemaker
+╠═════════════════════════      
+║        *🔥𝙼𝙴𝙳𝙸𝙰🔥*
+║  
+║
+║  ➩ ${prefix}trendtwit
+║  ➩ ${prefix}randomkpop
+║   【FOTOS ALEATÓRIA DE KPOP】
+║  ➩ ${prefix}ytsearch
+║    【PESQUISA】
+╠═════════════════════════
+║        *🔥𝙴𝙳𝚄𝙲𝙰𝙲𝙰𝙾🔥* 
+║  
+║  ➩ ${prefix}wiki
+║  ➩ ${prefix}wikien
+║  ➩ ${prefix}nulis
+║  ➩ ${prefix}quotes
+║  ➩ ${prefix}quotes2
+║  ➩ ${prefix}artinama
+╠═════════════════════════
+║      *🔥𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙴𝚁🔥* 
+║  
+║  ➩ ${prefix}images
+║   【IMAGENS ALEATÓRIA】
+║  ➩ ${prefix}ytmp3
+║    【MP3】
+║  ➩ ${prefix}ytmp4
+║    【DOWNLOAD MP4(VIDEO)】
+║  ➩ ${prefix}tiktok
+║  ➩ ${prefix}joox
+╠═════════════════════════        
+║           *🔥𝙼𝙴𝙼𝙴🔥*
+║
+║  ➩  ${prefix}meme
+║  ➩ 【MEME INDONÉSIO】
+║  ➩  ${prefix}memeindo
+║  ➩ 【MEMEINDO INDONESIO】╠═════════════════════════
+║      *🔥𝙼𝚄𝚂𝙸𝙲𝙰🔥* 
+║  
+║  ➩ ${prefix}lirik
+║  ➩ ${prefix}chord
+╠═════════════════════════
+║        *🔥𝙸𝚂𝙻𝙰𝙼🔥*
+║  
+║   ➩ ${prefix}quran
+╠═════════════════════════
+║        *🔥𝚂𝚃𝙰𝙻𝙺🔥* 
+║ 
+║  ➩ ${prefix}tiktokstalk
+║    【stalkear tik tok】
+║ ➩ ${prefix}igstalk
+╠═════════════════════════
+║       *🔥𝚆𝙸𝙱𝚄🔥* 
+║ 
+║  ➩ ${prefix}neonime
+║  ➩ ${prefix}pokemon
+║  ➩ ${prefix}loli
+║  ➩ ${prefix}waifu
+║  ➩ ${prefix}randomanime
+║  ➩ ${prefix}husbu
+║  ➩ ${prefix}husbu2
+║  ➩ ${prefix}wait
+║  ➩ ${prefix}nekonime
+╠═════════════════════════
+║       *🔥𝙳𝙸𝚅𝙴𝚁𝚂𝙰𝙾🔥* 
+║  
+║  ➩ ${prefix}alay
+║  ➩ ${prefix}gantengcek
+║  ➩ ${prefix}watak
+║  ➩ ${prefix}hobby
+║  ➩ ${prefix}game
+║  ➩ ${prefix}bucin
+║  ➩ ${prefix}trust
+║  ➩ ${prefix}dare
+║  ➩ ${prefix}simal
+╠═════════════════════════
+║          *🔥𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙰𝙾🔥* 
+║  
+║  ➩ ${prefix}bahasa
+║  【SABER TODOS CÓDIGO DE PAIS】
+║  ➩ ${prefix}kodenegara
+║  ➩ ${prefix}kbbi
+║  ➩ ${prefix}fakta
+║  ➩ ${prefix}infocuaca
+║  ➩ ${prefix}infogempa
+║  ➩ ${prefix}jadwaltvnow
+║  ➩ ${prefix}covid
+╠═════════════════════════
+║  *🔥𝚂𝙾 𝙵𝚄𝙽𝙲𝙸𝙾𝙽𝙰 𝙲𝙾𝙼 𝙾 𝙳𝙾𝙽𝙾🔥* 
+║
+║   ➨ ${prefix}setprefix
+║     【MUDAR PREFIXO】
+║   ➨ ${prefix}block
+║     【BLOQUEAR】
+║   ➨ ${prefix}bc
+║     【TM】
+║   ➨ ${prefix}bcgc
+║    【TM SÓ PARA MEMBROS DO GP】
+║   ➨ ${prefix}clearall
+║     【APAGAR TODAS MSG】
+╠═════════════════════════
+║ㅤㅤㅤㅤㅤ
+║ㅤㅤㅤ*🔥𝐻𝐸𝑁𝑅𝐼𝑄𝑈𝐸 𝐷𝑂𝑀𝐼𝑁𝐴🔥*
+║
+╚═════════════════════════
 `
 }
 
